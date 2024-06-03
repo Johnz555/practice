4ae93c7d13a6807c924a0784791ebb4c1e78bf94
Johnz555 <7167166@MAIL.RU>
2024-06-03 21:53:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# word length
+a = 'Hello, Wold!'
+print(len(a))
+# sums and differences
+first = 89
+second = 134
+summa = first+second
+diff = first-second
+print(summa)
+print(diff)
+# arithmetic mean
+first = 23
+second = 34
+third = 45
+mean = (first+second+third)//3
+print(mean)
+# simple strings
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string+", "+first_string)
+# A complex formula
+a = 35
+b = 47
+c = 15
+f = ((a*b)+(a*c))**3/2
+print(f)
