c1325ffaa296824cd218eb693739aa64b01c018f
Johnz555 <7167166@MAIL.RU>
2024-06-05 14:15:21 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,6 @@
+homework = 12
+number_of_hours = 1.5
+name_of_the_course = "Python"
+time_for_one_task = number_of_hours / homework
+print("Курс: ", name_of_the_course, ", всего задач:", homework, ", затрачено часов: ", number_of_hours, ", среднее время выполнения: ", time_for_one_task, "часа")
+
