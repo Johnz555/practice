a24eb5abb02a136ffca8cb9a11e2ac420b7c29fe
Johnz555 <7167166@MAIL.RU>
2024-06-13 20:56:54 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,6 @@
 first = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
-third = int(input('Введите nhtnmt число: '))
+third = int(input('Введите третье число: '))
 if first == second and second == third:
     print(3)
 elif first == second or first == third or second == third:
